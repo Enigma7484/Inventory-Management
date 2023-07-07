99f10abbf451d7a52494ce0f8b4568127ddaf7f1
Omar Shamsul Haque <omarhaque7484@gmail.com>
2023-07-07 13:53:56 +0600

    Commit 2
    P2 of 5

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleware/errorMiddleware")
 
 const app = express();
 
@@ -20,13 +21,15 @@ app.get("/", (req, res) => {
     res.send("Home Page");
 });
 
+// Error Middleware
+app.use(errorHandler);
 // Connect to DB and start server
 const PORT = process.env.PORT || 5000;
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`)
+            console.log(`Server running on port ${PORT}.`)
         })
 
     })
