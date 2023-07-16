50302e2f0dc74d689ddf0fb1660ea77933f9509a
Omar Shamsul Haque <omarhaque7484@gmail.com>
2023-07-16 20:01:27 +0600

    Commit 4
    Backend Complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,9 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const Token = require("../models/tokenModel");
+const crypto = require("crypto");
+const sendEmail = require("../utils/sendEmail");
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
@@ -57,13 +60,10 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error("Invalid user data");
     }
-
-
-
 });
 
 // Login User
-const loginUser = asyncHandler (async (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
 
     const { email, password } = req.body;
 
@@ -121,7 +121,7 @@ const logout = asyncHandler(async (req, res) => {
 });
 
 // Get User Data
-const getUser = asyncHandler (async (req, res) => {
+const getUser = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id);
     if (user) {
         const { _id, name, email, photo, phone, bio } = user;
@@ -155,9 +155,153 @@ const loginStatus = asyncHandler(async (req, res) => {
     return res.json(false);
 });
 
-const updateUser = asyncHandler (async (req, res) => {
-    res.send("User updated");
+// Update User
+const updateUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        const { _id, name, email, photo, phone, bio } = user;
+        user.email = email;
+        user.name = req.body.name || name;
+        user.phone = req.body.phone || phone;
+        user.bio = req.body.bio || bio;
+        user.photo = req.body.photo || photo;
+
+        const updatedUser = await user.save();
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            photo: updatedUser.photo,
+            phone: updatedUser.phone,
+            bio: updatedUser.bio,
+        })
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
 })
+
+const changePassword = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    const { oldPassword, password } = req.body;
+
+    if (!user) {
+        res.status(400);
+        throw new Error("User not found, please sign up");
+    }
+    // Validate
+    if (!oldPassword || !password) {
+        res.status(400);
+        throw new Error("Please add old and new password");
+    }
+
+    // Check if old password matches password in the DB
+    const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
+
+    // Save new password
+    if (user && passwordIsCorrect) {
+        user.password = password;
+        await user.save();
+        res.status(200).send("Password change successful");
+    } else {
+        res.status(400);
+        throw new Error("Old password is incorrect");
+    }
+})
+
+const forgotPassword = asyncHandler(async (req, res) => {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        res.status(404);
+        throw new Error("User does not exist");
+    }
+
+    // Delete token if it exists in DB
+    let token = await Token.findOne({ userId: user._id });
+    if (token) {
+        await token.deleteOne();
+    }
+
+    // Create Reset Token
+    let resetToken = crypto.randomBytes(32).toString("hex") + user._id;
+    console.log(resetToken);
+
+    // Hash token before saving to DB
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex");
+
+    // Save Token to DB
+    await new Token({
+        userId: user._id,
+        token: hashedToken,
+        createdAt: Date.now(),
+        expiresAt: Date.now() + 30 * (60 * 1000) // Thirty minutes
+    }).save()
+
+    // Construct Reset Url
+    const resetUrl = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
+
+    // Reset Email
+    const message = `
+        <h2>Hello ${user.name}</h2>
+        <p>Please use the url below to reset your password</p>
+        <p>The reset link is valid for 30 minutes</p>
+
+        <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
+
+        <p>Regards...</p>
+        <p>Inv-Manage Team</p>
+    `;
+
+    const subject = "Password Reset Request";
+    const send_to = user.email;
+    const sent_from = process.env.EMAIL_USER;
+
+    try {
+        await sendEmail(subject, message, send_to, sent_from);
+        res.status(200).json({ success: true, message: "Reset Email Sent" });
+    } catch (err) {
+        res.status(500);
+        throw new Error("Email not sent, please try again");
+    }
+});
+
+// Reset Password
+const resetPassword = asyncHandler(async (req, res) => {
+
+    const { password } = req.body;
+    const { resetToken } = req.params;
+
+    // Hash token, then compare to Token in DB
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(resetToken)
+        .digest("hex");
+
+    // Find Token in DB
+    const userToken = await Token.findOne({
+        token: hashedToken,
+        expiresAt: { $gt: Date.now() }
+    })
+
+    if (!userToken) {
+        res.status(404);
+        throw new Error("Invalid or Expired Token");
+    }
+
+    // Find user
+    const user = await User.findOne({ _id: userToken.userId });
+    user.password = password;
+    await user.save();
+    res.status(200).json({
+        message: "Password Reset Successful, Please Login"
+    });
+});
 
 module.exports = {
     registerUser,
@@ -166,4 +310,7 @@ module.exports = {
     getUser,
     loginStatus,
     updateUser,
+    changePassword,
+    forgotPassword,
+    resetPassword,
 };
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 
 const protect = asyncHandler(async (req, res, next) => {
   try {
-    console.log(req.cookie)
     const token = req.cookies.token;
     if (!token) {
       res.status(401);
